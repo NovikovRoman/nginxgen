eb04a522d313e465a722f843e4ae82df12d210bd
Novikov Roman <service.for.roman@gmail.com>
2021-02-17 16:12:43 +0300

    fix info.go

--- info.go
+++ info.go
@@ -1,25 +1,11 @@
 package main
 
-func aboutLogFormat(proxyDomain string) (res string) {
-	var hr = "---------------------------------------"
-	res = hr + "\nДобавить в /etc/hosts:\n127.0.0.1    " + proxyDomain + "\nили заменить на свой backend host\n" + hr
-	res += `
-Добавить в nginx.conf:
-http {
-	…
-	proxy_cache_path /var/cache/nginx levels=1:2 keys_zone=all:32m max_size=1g inactive=60m use_temp_path=off;
-	…
-	##
-	# Logging Settings
-	##
-	log_format nginxgen_proxylog '$http_x_forwarded_for - $remote_addr [$time_local] '
-            '"$request" $status $body_bytes_sent "$http_referer" '
-            '"$http_user_agent"';
-	…
-}
-
-Перезапустите nginx:
-service nginx restart
-`
+func aboutLogFormat(domain, proxyDomain, root string) (res string) {
+	domainConf := domain + ".conf"
+	hr := "---------------------------------------"
+	res =  "\n\033[0;31mНастройте `nginx.conf`. Для SSL установите `dehydrated`. Подробнее в README.md.\nЕсли это первая генерация, то перенесите сниппеты в nginx:\033[0m\nsudo mv " + root + "nginxgen/snippets/* /etc/nginx/snippets/\n" + hr
+	res += "\n\n\033[0;32mДобавить в /etc/hosts:\033[0m\n127.0.0.1    " + proxyDomain + "\nили заменить на свой backend host\n" + hr
+	res += "\n\n\033[0;32mСкопировать конфиг:\033[0m\nsudo cp " + root + "nginxgen/" + domainConf + " /etc/nginx/sites-available/\n\n"
+	res += "\033[0;32mСоздать символическую ссылку:\033[0m\nsudo ln -s /etc/nginx/sites-available/" + domainConf + " /etc/nginx/sites-enabled/" + domain + "\n"
 	return res + hr
 }
